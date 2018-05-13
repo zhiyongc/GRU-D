4d43db8376a6a102e5f718b091267cdae3979abf
unknown <zhiyongc@uw.edu>
2018-05-12 17:13:20 -0700

    upload code

---
+++ GRUD.py
@@ -0,0 +1,168 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 12 16:48:54 2018
+
+@author: Zhiyong
+"""
+
+import torch.utils.data as utils
+import torch.nn.functional as F
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+from torch.nn.parameter import Parameter
+import math
+import numpy as np
+import pandas as pd
+import time
+
+class FilterLinear(nn.Module):
+    def __init__(self, in_features, out_features, filter_square_matrix, bias=True):
+        '''
+        filter_square_matrix : filter square matrix, whose each elements is 0 or 1.
+        '''
+        super(FilterLinear, self).__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+        
+        use_gpu = torch.cuda.is_available()
+        self.filter_square_matrix = None
+        if use_gpu:
+            self.filter_square_matrix = Variable(filter_square_matrix.cuda(), requires_grad=False)
+        else:
+            self.filter_square_matrix = Variable(filter_square_matrix, requires_grad=False)
+        
+        self.weight = Parameter(torch.Tensor(out_features, in_features))
+        if bias:
+            self.bias = Parameter(torch.Tensor(out_features))
+        else:
+            self.register_parameter('bias', None)
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        stdv = 1. / math.sqrt(self.weight.size(1))
+        self.weight.data.uniform_(-stdv, stdv)
+        if self.bias is not None:
+            self.bias.data.uniform_(-stdv, stdv)
+#         print(self.weight.data)
+#         print(self.bias.data)
+
+    def forward(self, input):
+#         print(self.filter_square_matrix.mul(self.weight))
+        return F.linear(input, self.filter_square_matrix.mul(self.weight), self.bias)
+
+    def __repr__(self):
+        return self.__class__.__name__ + '(' \
+            + 'in_features=' + str(self.in_features) \
+            + ', out_features=' + str(self.out_features) \
+            + ', bias=' + str(self.bias is not None) + ')'
+        
+class GRUD(nn.Module):
+    def __init__(self, input_size, cell_size, hidden_size, X_mean, output_last = False):
+        """
+        Recurrent Neural Networks for Multivariate Times Series with Missing Values
+        GRU-D: GRU exploit two representations of informative missingness patterns, i.e., masking and time interval.
+        cell_size is the size of cell_state.
+        
+        Implemented based on the paper: 
+        @article{che2018recurrent,
+          title={Recurrent neural networks for multivariate time series with missing values},
+          author={Che, Zhengping and Purushotham, Sanjay and Cho, Kyunghyun and Sontag, David and Liu, Yan},
+          journal={Scientific reports},
+          volume={8},
+          number={1},
+          pages={6085},
+          year={2018},
+          publisher={Nature Publishing Group}
+        }
+        
+        GRU-D:
+            input_size: variable dimension of each time
+            hidden_size: dimension of hidden_state
+            mask_size: dimension of masking vector
+            X_mean: the mean of the historical input data
+        """
+        
+        super(GRUD, self).__init__()
+        
+        self.hidden_size = hidden_size
+        self.delta_size = input_size
+        self.mask_size = input_size
+        
+        use_gpu = torch.cuda.is_available()
+        if use_gpu:
+            self.identity = torch.eye(input_size).cuda()
+            self.zeros = Variable(torch.zeros(input_size).cuda())
+            self.X_mean = Variable(torch.Tensor(X_mean).cuda())
+        else:
+            self.identity = torch.eye(input_size)
+            self.zeros = Variable(torch.zeros(input_size))
+            self.X_mean = Variable(torch.Tensor(X_mean))
+        
+        self.zl = nn.Linear(input_size + hidden_size + self.mask_size, hidden_size)
+        self.rl = nn.Linear(input_size + hidden_size + self.mask_size, hidden_size)
+        self.hl = nn.Linear(input_size + hidden_size + self.mask_size, hidden_size)
+        
+        self.gamma_x_l = FilterLinear(self.delta_size, self.delta_size, self.identity)
+        
+        self.gamma_h_l = nn.Linear(self.delta_size, self.delta_size)
+        
+        self.output_last = output_last
+        
+    def step(self, x, h, mask, delta, x_mean):
+        
+        batch_size = x.shape[0]
+        dim_size = x.shape[1]
+        
+        delta_x = torch.exp(-torch.max(self.zeros, self.gamma_x_l(delta)))
+        delta_h = torch.exp(-torch.max(self.zeros, self.gamma_h_l(delta)))
+        
+        x = mask * x + (1 - mask) * ( delta_x * x + (1 - delta_x) * x_mean)
+        h = delta_h * h
+        
+        combined = torch.cat((x, h, mask), 1)
+        z = F.sigmoid(self.zl(combined))
+        r = F.sigmoid(self.rl(combined))
+        h_tilde = F.tanh(self.hl(combined))
+        h = (1 - z) * h + z * h_tilde
+        
+        return h
+    
+    def forward(self, input):
+        batch_size = input.size(0)
+        type_size = input.size(1)
+        step_size = input.size(2)
+        spatial_size = input.size(3)
+        
+        Hidden_State = self.initHidden(batch_size)
+        X = torch.squeeze(input[:,0,:,:])
+        X_last_obsv = torch.squeeze(input[:,1,:,:])
+        Mask = torch.squeeze(input[:,2,:,:])
+        Delta = torch.squeeze(input[:,3,:,:])
+        
+        outputs = None
+        for i in range(step_size):
+            Hidden_State = self.step(torch.squeeze(X[:,i:i+1,:])\
+                                     , Hidden_State\
+                                     , torch.squeeze(Mask[:,i:i+1,:])\
+                                     , torch.squeeze(Delta[:,i:i+1,:])\
+                                     , torch.squeeze(self.X_mean[:,i:i+1,:]))  
+            if outputs is None:
+                outputs = Hidden_State.unsqueeze(1)
+            else:
+                outputs = torch.cat((Hidden_State.unsqueeze(1), outputs), 1)
+                
+        if self.output_last:
+            return outputs[:,-1,:]
+        else:
+            return outputs
+    
+    def initHidden(self, batch_size):
+        use_gpu = torch.cuda.is_available()
+        if use_gpu:
+            Hidden_State = Variable(torch.zeros(batch_size, self.hidden_size).cuda())
+            return Hidden_State
+        else:
+            Hidden_State = Variable(torch.zeros(batch_size, self.hidden_size))
+            return Hidden_State
+        
---
+++ main.py
@@ -0,0 +1,362 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 12 16:49:49 2018
+
+@author: Zhiyong
+"""
+
+from GRUD import * 
+
+def PrepareDataset(speed_matrix, \
+                   BATCH_SIZE = 40, \
+                   seq_len = 10, \
+                   pred_len = 1, \
+                   train_propotion = 0.7, \
+                   valid_propotion = 0.2, \
+                   masking = False, \
+                   mask_ones_proportion = 0.8):
+    """ Prepare training and testing datasets and dataloaders.
+    
+    Convert speed/volume/occupancy matrix to training and testing dataset. 
+    The vertical axis of speed_matrix is the time axis and the horizontal axis 
+    is the spatial axis.
+    
+    Args:
+        speed_matrix: a Matrix containing spatial-temporal speed data for a network
+        seq_len: length of input sequence
+        pred_len: length of predicted sequence
+    Returns:
+        Training dataloader
+        Testing dataloader
+    """
+    time_len = speed_matrix.shape[0]
+    
+    speed_matrix = speed_matrix.clip(0, 100)
+    
+    max_speed = speed_matrix.max().max()
+    speed_matrix =  speed_matrix / max_speed
+    
+    speed_sequences, speed_labels = [], []
+    for i in range(time_len - seq_len - pred_len):
+        speed_sequences.append(speed_matrix.iloc[i:i+seq_len].values)
+        speed_labels.append(speed_matrix.iloc[i+seq_len:i+seq_len+pred_len].values)
+    speed_sequences, speed_labels = np.asarray(speed_sequences), np.asarray(speed_labels)
+    
+    # using zero-one mask to randomly set elements to zeros
+    if masking:
+        print('Split Speed finished. Start to generate Mask, Delta, Last_observed_X ...')
+        np.random.seed(1024)
+        Mask = np.random.choice([0,1], size=(speed_sequences.shape), p = [1 - mask_ones_proportion, mask_ones_proportion])
+        speed_sequences = np.multiply(speed_sequences, Mask)
+        
+        # temporal information
+        interval = 5 # 5 minutes
+        S = np.zeros_like(speed_sequences) # time stamps
+        for i in range(S.shape[1]):
+            S[:,i,:] = interval * i
+
+        Delta = np.zeros_like(speed_sequences) # time intervals
+        for i in range(1, S.shape[1]):
+            Delta[:,i,:] = S[:,i,:] - S[:,i-1,:]
+
+        missing_index = np.where(Mask == 0)
+
+        X_last_obsv = speed_sequences
+        for idx in range(missing_index[0].shape[0]):
+            i = missing_index[0][idx]
+            j = missing_index[1][idx]
+            k = missing_index[2][idx]
+            if j != 0:
+                Delta[i,j,k] = Delta[i,j,k] + Delta[i,j-1,k]
+                X_last_obsv[i,j,k] = X_last_obsv[i,j-1,k] # last observation
+        Delta = Delta / Delta.max() # normalize
+    
+    # shuffle and split the dataset to training and testing datasets
+    print('Generate Mask, Delta, Last_observed_X finished. Start to shuffle and split dataset ...')
+    sample_size = speed_sequences.shape[0]
+    index = np.arange(sample_size, dtype = int)
+    np.random.seed(1024)
+    np.random.shuffle(index)
+    
+    speed_sequences = speed_sequences[index]
+    speed_labels = speed_labels[index]
+    
+    if masking:
+        X_last_obsv = X_last_obsv[index]
+        Mask = Mask[index]
+        Delta = Delta[index]
+        speed_sequences = np.expand_dims(speed_sequences, axis=1)
+        X_last_obsv = np.expand_dims(X_last_obsv[index], axis=1)
+        Mask = np.expand_dims(Mask[index], axis=1)
+        Delta = np.expand_dims(Delta[index], axis=1)
+        dataset_agger = np.concatenate((speed_sequences, X_last_obsv, Mask, Delta), axis = 1)
+        
+    train_index = int(np.floor(sample_size * train_propotion))
+    valid_index = int(np.floor(sample_size * ( train_propotion + valid_propotion)))
+    
+    if masking:
+        train_data, train_label = dataset_agger[:train_index], speed_labels[:train_index]
+        valid_data, valid_label = dataset_agger[train_index:valid_index], speed_labels[train_index:valid_index]
+        test_data, test_label = dataset_agger[valid_index:], speed_labels[valid_index:]
+    else:
+        train_data, train_label = speed_sequences[:train_index], speed_labels[:train_index]
+        valid_data, valid_label = speed_sequences[train_index:valid_index], speed_labels[train_index:valid_index]
+        test_data, test_label = speed_sequences[valid_index:], speed_labels[valid_index:]
+    
+    train_data, train_label = torch.Tensor(train_data), torch.Tensor(train_label)
+    valid_data, valid_label = torch.Tensor(valid_data), torch.Tensor(valid_label)
+    test_data, test_label = torch.Tensor(test_data), torch.Tensor(test_label)
+    
+    train_dataset = utils.TensorDataset(train_data, train_label)
+    valid_dataset = utils.TensorDataset(valid_data, valid_label)
+    test_dataset = utils.TensorDataset(test_data, test_label)
+    
+    train_dataloader = utils.DataLoader(train_dataset, batch_size = BATCH_SIZE, shuffle=True, drop_last = True)
+    valid_dataloader = utils.DataLoader(valid_dataset, batch_size = BATCH_SIZE, shuffle=True, drop_last = True)
+    test_dataloader = utils.DataLoader(test_dataset, batch_size = BATCH_SIZE, shuffle=True, drop_last = True)
+    
+    X_mean = np.mean(speed_sequences, axis = 0)
+    
+    print('Finished')
+    
+    return train_dataloader, valid_dataloader, test_dataloader, max_speed, X_mean
+
+def Train_Model(model, train_dataloader, valid_dataloader, num_epochs = 300, patience = 10, min_delta = 0.00001):
+    
+    print('Model Structure: ', model)
+    print('Start Training ... ')
+    
+    model.cuda()
+    
+    if (type(model) == nn.modules.container.Sequential):
+        output_last = model[-1].output_last
+        print('Output type dermined by the last layer')
+    else:
+        output_last = model.output_last
+        print('Output type dermined by the model')
+        
+    loss_MSE = torch.nn.MSELoss()
+    loss_L1 = torch.nn.L1Loss()
+    
+    learning_rate = 0.0001
+    optimizer = torch.optim.RMSprop(model.parameters(), lr = learning_rate, alpha=0.99)
+    use_gpu = torch.cuda.is_available()
+    
+    interval = 100
+    losses_train = []
+    losses_valid = []
+    losses_epochs_train = []
+    losses_epochs_valid = []
+    
+    cur_time = time.time()
+    pre_time = time.time()
+    
+    # Variables for Early Stopping
+    is_best_model = 0
+    patient_epoch = 0
+    for epoch in range(num_epochs):
+        
+        trained_number = 0
+        
+        valid_dataloader_iter = iter(valid_dataloader)
+        
+        losses_epoch_train = []
+        losses_epoch_valid = []
+        
+        for data in train_dataloader:
+            inputs, labels = data
+
+            if inputs.shape[0] != batch_size:
+                continue
+
+            if use_gpu:
+                inputs, labels = Variable(inputs.cuda()), Variable(labels.cuda())
+            else: 
+                inputs, labels = Variable(inputs), Variable(labels)
+            
+            model.zero_grad()
+
+            outputs = model(inputs)
+            
+            if output_last:
+                loss_train = loss_MSE(outputs, labels)
+            else:
+                full_labels = torch.cat((inputs[:,1:,:], labels), dim = 1)
+                loss_train = loss_MSE(outputs, full_labels)
+        
+            losses_train.append(loss_train.data)
+            losses_epoch_train.append(loss_train.data)
+            
+            optimizer.zero_grad()
+            
+            loss_train.backward()
+            
+            optimizer.step()
+            
+             # validation 
+            try: 
+                inputs_val, labels_val = next(valid_dataloader_iter)
+            except StopIteration:
+                valid_dataloader_iter = iter(valid_dataloader)
+                inputs_val, labels_val = next(valid_dataloader_iter)
+            
+            if use_gpu:
+                inputs_val, labels_val = Variable(inputs_val.cuda()), Variable(labels_val.cuda())
+            else: 
+                inputs_val, labels_val = Variable(inputs_val), Variable(labels_val)
+                
+            model.zero_grad()
+            
+            outputs_val = model(inputs_val)
+            
+            if output_last:
+                loss_valid = loss_MSE(outputs_val, labels_val)
+            else:
+                full_labels_val = torch.cat((inputs_val[:,1:,:], labels_val), dim = 1)
+                loss_valid = loss_MSE(outputs_val, full_labels_val)
+
+            losses_valid.append(loss_valid.data)
+            losses_epoch_valid.append(loss_valid.data)
+            
+            # output
+            trained_number += 1
+            
+        avg_losses_epoch_train = sum(losses_epoch_train).cpu().numpy()[0] / float(len(losses_epoch_train))
+        avg_losses_epoch_valid = sum(losses_epoch_valid).cpu().numpy()[0] / float(len(losses_epoch_valid))
+        losses_epochs_train.append(avg_losses_epoch_train)
+        losses_epochs_valid.append(avg_losses_epoch_valid)
+        
+        # Early Stopping
+        if epoch == 0:
+            is_best_model = 1
+            best_model = model
+            min_loss_epoch_valid = 10000.0
+            if avg_losses_epoch_valid < min_loss_epoch_valid:
+                min_loss_epoch_valid = avg_losses_epoch_valid
+        else:
+            if min_loss_epoch_valid - avg_losses_epoch_valid > min_delta:
+                is_best_model = 1
+                best_model = model
+                min_loss_epoch_valid = avg_losses_epoch_valid 
+                patient_epoch = 0
+            else:
+                is_best_model = 0
+                patient_epoch += 1
+                if patient_epoch >= patience:
+                    print('Early Stopped at Epoch:', epoch)
+                    break
+        
+        # Print training parameters
+        cur_time = time.time()
+        print('Epoch: {}, train_loss: {}, valid_loss: {}, time: {}, best model: {}'.format( \
+                    epoch, \
+                    np.around(avg_losses_epoch_train, decimals=8),\
+                    np.around(avg_losses_epoch_valid, decimals=8),\
+                    np.around([cur_time - pre_time] , decimals=2),\
+                    is_best_model) )
+        pre_time = cur_time
+                
+    return best_model, [losses_train, losses_valid, losses_epochs_train, losses_epochs_valid]
+
+
+def Test_Model(model, test_dataloader, max_speed):
+    
+    if (type(model) == nn.modules.container.Sequential):
+        output_last = model[-1].output_last
+    else:
+        output_last = model.output_last
+    
+    inputs, labels = next(iter(test_dataloader))
+    [batch_size, type_size, step_size, fea_size] = inputs.size()
+
+    cur_time = time.time()
+    pre_time = time.time()
+    
+    use_gpu = torch.cuda.is_available()
+    
+    loss_MSE = torch.nn.MSELoss()
+    loss_L1 = torch.nn.MSELoss()
+    
+    tested_batch = 0
+    
+    losses_mse = []
+    losses_l1 = [] 
+    MAEs = []
+    MAPEs = []
+
+    
+    for data in test_dataloader:
+        inputs, labels = data
+        
+        if inputs.shape[0] != batch_size:
+            continue
+    
+        if use_gpu:
+            inputs, labels = Variable(inputs.cuda()), Variable(labels.cuda())
+        else: 
+            inputs, labels = Variable(inputs), Variable(labels)
+        
+        outputs = model(inputs)
+    
+        loss_MSE = torch.nn.MSELoss()
+        loss_L1 = torch.nn.L1Loss()
+        
+        if output_last:
+            loss_mse = loss_MSE(outputs, labels)
+            loss_l1 = loss_L1(outputs, labels)
+            MAE = torch.mean(torch.abs(outputs - torch.squeeze(labels)))
+            MAPE = torch.mean(torch.abs(outputs - torch.squeeze(labels)) / torch.squeeze(labels))
+        else:
+            loss_mse = loss_MSE(outputs[:,-1,:], labels)
+            loss_l1 = loss_L1(outputs[:,-1,:], labels)
+            MAE = torch.mean(torch.abs(outputs[:,-1,:] - torch.squeeze(labels)))
+            MAPE = torch.mean(torch.abs(outputs[:,-1,:] - torch.squeeze(labels)) / torch.squeeze(labels))
+            
+        losses_mse.append(loss_mse.data)
+        losses_l1.append(loss_l1.data)
+        MAEs.append(MAE.data)
+        MAPEs.append(MAPE.data)
+        
+        tested_batch += 1
+    
+        if tested_batch % 1000 == 0:
+            cur_time = time.time()
+            print('Tested #: {}, loss_l1: {}, loss_mse: {}, time: {}'.format( \
+                  tested_batch * batch_size, \
+                  np.around([loss_l1.data[0]], decimals=8), \
+                  np.around([loss_mse.data[0]], decimals=8), \
+                  np.around([cur_time - pre_time], decimals=8) ) )
+            pre_time = cur_time
+    losses_l1 = np.array(losses_l1)
+    losses_mse = np.array(losses_mse)
+    MAEs = np.array(MAEs)
+    MAPEs = np.array(MAPEs)
+    
+    mean_l1 = np.mean(losses_l1) * max_speed
+    std_l1 = np.std(losses_l1) * max_speed
+    MAE_ = np.mean(MAEs) * max_speed
+    MAPE_ = np.mean(MAPEs) * 100
+    
+    print('Tested: L1_mean: {}, L1_std: {}, MAE: {} MAPE: {}'.format(mean_l1, std_l1, MAE_, MAPE_))
+    return [losses_l1, losses_mse, mean_l1, std_l1]
+
+
+if __name__ == "__main__":
+    
+    data = 'loop'
+    if data == 'inrix':
+        speed_matrix =  pd.read_pickle('../Data_Warehouse/Data_network_traffic/inrix_seattle_speed_matrix_2012')
+    elif data == 'loop':
+        speed_matrix =  pd.read_pickle('../Data_Warehouse/Data_network_traffic//speed_matrix_2015')
+        
+    train_dataloader, valid_dataloader, test_dataloader, max_speed, X_mean = PrepareDataset(speed_matrix, BATCH_SIZE = 64, masking = True)
+    
+    inputs, labels = next(iter(train_dataloader))
+    [batch_size, type_size, step_size, fea_size] = inputs.size()
+    input_dim = fea_size
+    hidden_dim = fea_size
+    output_dim = fea_size
+    
+    grud = GRUD(input_dim, hidden_dim, output_dim, X_mean, output_last = True)
+    best_grud, losses_grud = Train_Model(grud, train_dataloader, valid_dataloader)
+    [losses_l1, losses_mse, mean_l1, std_l1] = Test_Model(best_grud, test_dataloader, max_speed)
